256ab7d061ad299a2756fc5830dff4d53006db4d
Zadigo <pendenquejohn@gmail.com>
2024-05-10 10:07:18 +0200

    TableExistsError / FieldExistsError

--- lorelie/exceptions.py
+++ lorelie/exceptions.py
@@ -1,3 +1,22 @@
 class ImproperlyConfiguredError(Exception):
     def __init__(self, table, message):
         super().__init__(message)
+
+
+class TableExistsError(Exception):
+    def __init__(self, name):
+        message = (
+            f'Table with name "{name}" '
+            'does not exist in the database'
+        )
+        super().__init__(message)
+
+
+class FieldExistsError(Exception):
+    def __init__(self, name, table):
+        available_fields = ', '.join(table.field_names)
+        message = (
+            f'Field with name "{name}" '
+            f'does not exist on the table: {available_fields}'
+        )
+        super().__init__(message)

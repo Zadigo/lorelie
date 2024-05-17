82f2bb9d51e50db3f6145534da66561f026300e5
Zadigo <pendenquejohn@gmail.com>
2024-05-17 12:10:18 +0200

    Code refactoring

--- _dev.py
+++ _dev.py
@@ -1,13 +1,16 @@
 import dataclasses
 
-from lorelie.aggregation import Count, Sum
+from lorelie.aggregation import (Avg, CoefficientOfVariation, Count, Max,
+                                 MeanAbsoluteDifference, Min, StDev, Sum,
+                                 Variance)
 from lorelie.backends import SQLiteBackend
 from lorelie.database.base import Database
 from lorelie.database.indexes import Index
 from lorelie.database.nodes import OrderByNode
 from lorelie.expressions import Q
 from lorelie.fields.base import CharField, DateTimeField, IntegerField, Value
-from lorelie.functions import Lower, SubStr, Upper, ExtractYear, ExtractMonth, ExtractDay, Concat
+from lorelie.functions import (Concat, ExtractDay, ExtractMonth, ExtractYear,
+                               Lower, SubStr, Upper)
 from lorelie.tables import Table
 
 table = Table(
@@ -21,8 +24,25 @@ table = Table(
     ]
 )
 
-db = Database(table, name='test_database')
+models = Table(
+    'models',
+    str_field='firstname',
+    fields=[
+        CharField('firstname'),
+        CharField('lastname')
+    ]
+)
+
+db = Database(table, models, name='test_database')
+db.foreign_key(table, models)
 db.migrate()
+
+item = db.objects.get('products', id__eq=2)
+print(item)
+# item.models_rel.create(firstname='Kendall', lastname='Jenner')
+# qs = item.models_rel.all()
+# print(qs)
+# print(qs.sql_statement)
 
 # @dataclasses.dataclass
 # class Product:
@@ -37,12 +57,21 @@ db.migrate()
 # product = db.objects.create('products', name='Jupe longue', price=0)
 # product = db.objects.create('products', name='Jupe longue', price=45)
 
+# qs = db.objects.annotate('products', Count('price'))
+# qs2 = qs.filter(name='Jupe longue').filter(id=2)
+# print(qs2)
+# print(qs2.sql_statement)
 
-# qs = db.objects.annotate('products', Max('name'))
-qs = db.objects.annotate('products')
-print(qs.values('id'))
-# print(qs.values('upper_name'))
-
-
-# from django.db.models import Model
-# m = Model.objects.annotate()
+# result = db.objects.aggregate(
+#     'products',
+#     Count('price'),
+#     Sum('price'),
+#     Avg('price'),
+#     MeanAbsoluteDifference('price'),
+#     Variance('price'),
+#     StDev('price'),
+#     CoefficientOfVariation('price'),
+#     Max('price'),
+#     Min('price')
+# )
+# print(result)

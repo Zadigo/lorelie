5a904f6693437cbbc67d8117dab6087aa15187de
Zadigo <pendenquejohn@gmail.com>
2024-05-22 18:35:08 +0200

    Improve tests

--- lorelie/test/testcases.py
+++ lorelie/test/testcases.py
@@ -2,7 +2,9 @@ import unittest
 from functools import cached_property, lru_cache
 
 from lorelie.backends import SQLiteBackend
+from lorelie.constraints import CheckConstraint, UniqueConstraint
 from lorelie.database.base import Database
+from lorelie.expressions import Q
 from lorelie.fields.base import CharField, IntegerField
 from lorelie.tables import Table
 
@@ -34,4 +36,31 @@ class LorelieTestCase(unittest.TestCase):
             CharField('name'),
             IntegerField('height', min_value=150, default=150)
         ])
+        return table
+
+    def create_constrained_table(self):
+        table = Table(
+            'celebrities', 
+            fields=[
+                CharField('name', null=True),
+                IntegerField('height', null=True)
+            ],
+            constraints=[
+                CheckConstraint('no_kendall', Q(name__ne='Kendall')),
+                CheckConstraint('height_over_150', Q(height__gte=150))
+            ]
+        )
+        return table
+
+    def create_unique_constrained_table(self):
+        table = Table(
+            'celebrities', 
+            fields=[
+                CharField('name', null=True),
+                IntegerField('height', null=True)
+            ],
+            constraints=[
+                UniqueConstraint('my_constraint', fields=['name', 'height'])
+            ]
+        )
         return table

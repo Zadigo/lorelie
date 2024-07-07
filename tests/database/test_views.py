ef6c19aadab2518d85eae7b11a88a9636b158534
Zadigo <pendenquejohn@gmail.com>
2024-07-07 18:09:00 +0200

    Improve tests

--- tests/database/test_views.py
+++ tests/database/test_views.py
@@ -6,7 +6,14 @@ from lorelie.test.testcases import LorelieTestCase
 class TestView(LorelieTestCase):
     def test_structure(self):
         db = self.create_database()
+
         view = View('my_view', db.objects.all('celebrities'))
         qs = view(db.get_table('celebrities'))
         self.assertIsInstance(qs, QuerySet)
-        # self.assertTrue()
+        
+        list(qs.all())
+
+        self.assertEqual(
+            qs.query.sql,
+            'select * from my_view;'
+        )

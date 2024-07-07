b37062c5240442198eb910da84e854bbc031713e
Zadigo <pendenquejohn@gmail.com>
2024-07-07 13:08:36 +0200

    Code refactoring

--- _dev.py
+++ _dev.py
@@ -1,17 +1,17 @@
 import asyncio
 import dataclasses
 
-from database.functions.text import Lower
-
 from lorelie import log_queries
 from lorelie.constraints import CheckConstraint, UniqueConstraint
 from lorelie.database import registry, triggers
 from lorelie.database.base import Database
+from lorelie.database.functions.text import Lower
 from lorelie.database.functions.window import Rank, Window
 from lorelie.database.indexes import Index
 from lorelie.expressions import Case, F, Q, When
 from lorelie.fields.base import CharField, DateTimeField, IntegerField
 from lorelie.tables import Table
+from lorelie.database.functions import Count
 
 table = Table('products', fields=[
     CharField('name'),
@@ -20,11 +20,10 @@ table = Table('products', fields=[
     index=[Index('my_index', fields=['name'], condition=Q(name='Kendall'))]
 )
 
-db = Database(table, name='products')
+db = Database(table, name='products', log_queries=True)
 db.migrate()
 
-
-db.objects.create('products', name='Jupe')
+db.objects.all('products')
 
 # db.objects.create('products', name='Jupe')
 # item = db.objects.first('products')
